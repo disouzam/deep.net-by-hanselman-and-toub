3cd52d8d2ba968cd8b5364dd472f48171c7316d8
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-05-29 09:28:16 -0300

    Custom implementation of a pretty simple thread pool
    - Ends at 13:26 of the video - https://youtu.be/R-z2Hv-7nxk?si=Pmnm9DnDCI4ZNCLw&t=806

--- async-await/Program.cs
+++ async-await/Program.cs
@@ -1,7 +1,9 @@
-﻿for (int i = 0; i < 1000; i++)
+﻿using System.Collections.Concurrent;
+
+for (int i = 0; i < 1000; i++)
 {
     int localIndex = i;  // Create a local copy
-    ThreadPool.QueueUserWorkItem(delegate
+    MyThreadPool.QueueUserWorkItem(delegate
     {
         Console.WriteLine($"Starting a thread...{localIndex}");
         Thread.Sleep(1000);
@@ -10,3 +12,26 @@
 
 Console.WriteLine("Execution completed. Press any key to exit.");
 Console.ReadLine();
+
+static class MyThreadPool
+{
+    private static readonly BlockingCollection<Action> _workItems = new BlockingCollection<Action>();
+
+    public static void QueueUserWorkItem(Action action) => _workItems.Add(action);
+
+    static MyThreadPool()
+    {
+        for (int i = 0; i < Environment.ProcessorCount; i++)
+        {
+            new Thread(() => 
+            {
+                while (true) 
+                {
+                    Action workItem = _workItems.Take();
+                    workItem();
+                }
+            }) 
+            { IsBackground = true }.Start();
+        }
+    }
+}
