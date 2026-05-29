afbeab1991e8fe877c84bfe01283e6f4599666fc
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-05-29 09:09:44 -0300

    First implementation of the use of ThreadPool to queue activities but incorrectly configured

--- async-await/Program.cs
+++ async-await/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿for (int i = 0; i<1000; i++)
+{
+    ThreadPool.QueueUserWorkItem(delegate
+    {
+        Console.WriteLine($"Starting a thread...{i}");
+        Thread.Sleep( 1000 );
+    });
+}
+
+Console.WriteLine("Execution completed. Press any key to exit.");
+Console.ReadLine();
